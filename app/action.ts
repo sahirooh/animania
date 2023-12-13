093712debaf9255bee60adb41d5b38be78a1e9ab
sahirooh <sahirooh@gmail.com>
2023-12-13 23:49:08 +0530

    updated action

--- app/action.ts
+++ app/action.ts
@@ -1,1 +1,9 @@
 "use server"
+
+export const fetchAnime = async () => {
+    const res = await fetch('https://shikimori.one/api/animes')
+
+    const data = await res.json()
+    console.log(data)
+    return data;
+}
