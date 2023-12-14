5a0137840559d3a2d748e5ac45081d87ef39d928
sahirooh <sahirooh@gmail.com>
2023-12-14 21:32:06 +0530

    added inview

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect } from "react";
+import {useInView} from "react-intersection-observer";
 
 function LoadMore() {
+
+  const {ref, inView} = useInView();
+
+  useEffect(() => {
+    if(inView){alert("Load More")}
+  },[inView])
+
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
